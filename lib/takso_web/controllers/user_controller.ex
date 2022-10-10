dbbba7f780a1f073620ae50bea734c1fc729450f
rufat <mrrufatabd@gmail.com>
2022-10-10 14:12:21 +0300

    Part 1: Adding users

--- lib/takso_web/controllers/user_controller.ex
+++ lib/takso_web/controllers/user_controller.ex
@@ -8,4 +8,16 @@ defmodule TaksoWeb.UserController do
     users = Repo.all(User)
     render(conn, "index.html", users: users)
   end
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{}, %{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+   def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+
+    Repo.insert(changeset)
+    redirect(conn, to: Routes.user_path(conn, :index))
+  end
 end
